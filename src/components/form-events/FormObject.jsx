dec83b578ebd45f7a0c171ba00174c3580834618
Bekir <bytgm06@gmail.com>
2024-09-12 19:44:53 +0300

    C

--- src/components/form-events/FormObject.jsx
+++ src/components/form-events/FormObject.jsx
@@ -1,84 +1,88 @@
 import { useState } from "react"
 
 
-export const FormObject= () => {
-const [info, setinfo] = useState({name:"", password:"",
-     country:""})
+export const FormObject = () => {
+  const [info, setinfo] = useState({
+    name: "", password: "",
+    country: ""
+  })
 
-    const handleInfo =(e)=>setinfo({...info, [e.target.id]: 
-        e.target.value})
+  const handleInfo = (e) => setinfo({
+    ...info, [e.target.id]:
+      e.target.value
+  })
 
-    const sendToDatabase= (e)=>{
-      e.preventDefault(); // submitin doğal halini engelledik
-      alert(`
+  const sendToDatabase = (e) => {
+    e.preventDefault(); // submitin doğal halini engelledik
+    alert(`
         name: ${info.name}
         password: ${info.password}
         contry: ${info.country}
      
         `)
 
-       
-        
+
+
   }
-    
+
   return (
     <div className="mt-4 p-3">
-        <div className="text-center">
-            <h1>*****************</h1>
-            <h2>Form-Events</h2>
-        </div>
-  <form onSubmit={sendToDatabase}>
-  <div className="mb-3">
-    <label htmlFor="name" className="form-label">
-      Name: <span className="text-danger">{info.name}</span> 
-    </label>
-    <input
-   
-      onChange={handleInfo}
-      type="text"
-      className="form-control"
-      id="name"
-      value={info.name}
-      // required in çalisması için submit butonu gerekir
-      required
-      
-    />
-    
-  </div>
-  <div className="mb-3">
-    <label htmlFor="password" className="form-label">
-      Password: <span className="text-danger">{info.password}</span>
-    </label>
-    <input
-      onChange={handleInfo}
-      type="password"
-      className="form-control"
-      id="password"
-      value={info.password}
-      required
-    />
-  </div>
-  
-  <div className="mb-3">
-    <label htmlFor="country" className="form-label">
-      Country: <span className="text-danger">{info.country}</span>
-    </label>
-    <select 
-    onChange={handleInfo}
-    id="country" className="form-select" 
-    aria-label="Default select example" >
-  <option selected="">COUNTRİES</option>
-  <option value="Türkiye">TÜRKİYE</option>
-  <option value="Almanya">GERMANY</option>
-  <option value="Amerika">USA</option>
-</select>
+      <div className="text-center">
+        <h1>*****************</h1>
+        <h2>Form-Events</h2>
+      </div>
+      <form onSubmit={sendToDatabase}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name: <span className="text-danger">{info.name}</span>
+          </label>
+          <input
 
-  </div>
-  
-  <button type="submit" className="btn btn-primary">
-    Submit
-  </button>
-</form>
+            onChange={handleInfo}
+            type="text"
+            className="form-control"
+            id="name"
+            value={info.name}
+            // required in çalisması için submit butonu gerekir
+            required
+
+          />
+
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password: <span className="text-danger">{info.password}</span>
+          </label>
+          <input
+            onChange={handleInfo}
+            type="password"
+            className="form-control"
+            id="password"
+            value={info.password}
+            required
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="country" className="form-label">
+            Country: <span className="text-danger">{info.country}</span>
+          </label>
+          <select
+            onChange={handleInfo}
+            id="country" className="form-select"
+            aria-label="Default select example" >
+            <option selected="">COUNTRİES</option>
+            <option value="Türkiye">TÜRKİYE</option>
+            <option value="Almanya">GERMANY</option>
+            <option value="Amerika">USA</option>
+          </select>
+
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
 
     </div>
   )
--- src/components/mouse-events/MouseEvent.jsx
+++ src/components/mouse-events/MouseEvent.jsx
@@ -5,55 +5,55 @@ const MouseEvent = () => {
     const [coordX, setcoordX] = useState("")
     const [coordY, setcoordY] = useState("")
 
-    const handleDoubleClick=(e)=>e.target.classList.toggle("bg-danger")
-    const handleMouseMove=(e)=>{
+    const handleDoubleClick = (e) => e.target.classList.toggle("bg-danger")
+    const handleMouseMove = (e) => {
         setcoordX(e.nativeEvent.offsetX)
         setcoordY(e.nativeEvent.offsetY)
     }
 
-  return (
-    <div className="container text-center d-flex flex-column align-items-center mt-4">
-    <h2 className="text-danger">MOUSE EVENTS</h2>
-    <div
-      onMouseOver={()=>setvisible(true)}
-      onMouseLeave={()=>setvisible(false)}
-      id="todo-1"
-      className="bg-success text-light w-50 p-4"
-    >
-      onMouseOver & onMouseLeave
-    </div>
-    {visible && <p>Mouse üzerimde</p> }
-    <div
-      onDoubleClick={handleDoubleClick}
-      id="todo-2"
-      className="bg-success text-light w-50 p-4 mt-4"
-    >
-      onDoubleClick
-    </div>
-    <div
-      onMouseMove={handleMouseMove}
-      id="todo-3"
-      className="bg-success text-light w-50 p-4 my-4"
-    >
-      <span>onMouseMove</span>
-    </div>
-    <div
-      onMouseOver={handleMouseMove}
-      id="todo-4"
-      className="bg-success text-light w-50 p-4 my-4"
-    >
-      <span>onMouseOver</span>
-    </div>
-    <div
-      onMouseEnter={handleMouseMove}
-      id="todo-5"
-      className="bg-success text-light w-50 p-4 my-4"
-    >
-      <span>onMouseEnter</span>
-    </div>
-    X:{coordX} and Y:{coordY}
-  </div>
-  )
+    return (
+        <div className="container text-center d-flex flex-column align-items-center mt-4">
+            <h2 className="text-danger">MOUSE EVENTS</h2>
+            <div
+                onMouseOver={() => setvisible(true)}
+                onMouseLeave={() => setvisible(false)}
+                id="todo-1"
+                className="bg-success text-light w-50 p-4"
+            >
+                onMouseOver & onMouseLeave
+            </div>
+            {visible && <p>Mouse üzerimde</p>}
+            <div
+                onDoubleClick={handleDoubleClick}
+                id="todo-2"
+                className="bg-success text-light w-50 p-4 mt-4"
+            >
+                onDoubleClick
+            </div>
+            <div
+                onMouseMove={handleMouseMove}
+                id="todo-3"
+                className="bg-success text-light w-50 p-4 my-4"
+            >
+                <span>onMouseMove</span>
+            </div>
+            <div
+                onMouseOver={handleMouseMove}
+                id="todo-4"
+                className="bg-success text-light w-50 p-4 my-4"
+            >
+                <span>onMouseOver</span>
+            </div>
+            <div
+                onMouseEnter={handleMouseMove}
+                id="todo-5"
+                className="bg-success text-light w-50 p-4 my-4"
+            >
+                <span>onMouseEnter</span>
+            </div>
+            X:{coordX} and Y:{coordY}
+        </div>
+    )
 }
 
 export default MouseEvent
